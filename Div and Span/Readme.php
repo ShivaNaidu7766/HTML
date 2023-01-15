9d6aa579aa0affb8369a61131901dff0d8a79729
ShivaNaidu7766 <118107558+ShivaNaidu7766@users.noreply.github.com>
2023-01-16 02:19:25 +0530

    Update Readme.php

--- Div and Span/Readme.php
+++ Div and Span/Readme.php
@@ -1,3 +1,3 @@
-this is the clear example of dive and span element in html
+This is the clear example of dive and span element in html
 
 NOTE: Div cannot be inside Span, because Div is a block element  
