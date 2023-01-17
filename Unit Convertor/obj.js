9cb37b6a74a635715fe3a66e25a135a8fa2ff11d
ShivaNaidu7766 <118107558+ShivaNaidu7766@users.noreply.github.com>
2023-01-18 00:17:46 +0530

    Add files via upload

---
+++ Unit Convertor/obj.js
@@ -0,0 +1,22 @@
+var units = {
+    "uMm":"0.001",
+    "uCm":"0.01",
+    "uM":"1",
+    "uMg":"0.000001",
+    "uG":"0.001",
+    "uKg":"1",
+    "uTon":"1000",
+  }
+  
+  
+  var abbrev = {
+
+    "uMm":"mm",
+    "uCm":"cm",
+    "uM":"m",
+    "uMg":"mg",
+    "uG":"g",
+    "uKg":"kg",
+    "uTon":"t",
+
+  }
