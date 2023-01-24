d25855515398d193d5270430e673ddc53ec5442c
ShivaNaidu7766 <118107558+ShivaNaidu7766@users.noreply.github.com>
2023-01-24 21:01:15 +0530

    Update Readme.php

--- TO-DO List/Readme.php
+++ TO-DO List/Readme.php
@@ -2,5 +2,5 @@ To do list
 enter the user data!
 keep tab on task is done or not
 completed tasks can be viewed as marked
-able to edit the taks after enter
-able to delete the entered taks
+able to edit the tasks after enter
+able to delete the entered tasks
